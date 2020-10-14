687fb15610991061b86f32910727ad8c26893d4e
Andres Pizarro <randypizarro09@outlook.com>
2020-10-14 13:18:04 +0200

    Lectura por caracteres

--- src/Leer_fichero.java
+++ src/Leer_fichero.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -6,11 +7,15 @@ public class Leer_fichero {
 	public void lee() {
 		
 		try {
+			//Creacion del lector de fichero
 			FileReader Alumnos = new FileReader ("C:\\Users\\randy\\AccesoADatos\\Alumno.txt");
+			//Creacion de la memoria intermedia
+			BufferedReader guardado = new BufferedReader(Alumnos);
 			//Se almacenan los datos en c, igualar a 0 para leer desde la primera posicion 
 			int c = 0;
 			//Mientras no llegue al final de los caracteres
 			while (c!=-1) {
+				//c recorrera el lector Alumnos
 				c=Alumnos.read();
 				//casting de lo que hay almacenado en c para que no imprima caracteres sueltos 
 				char letra =(char)c;
